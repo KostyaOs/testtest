628d9a4abd60660e0cdb81179af6de0f6bf78417
KostyaOs <coolhappygames@gmail.com>
2022-02-06 07:50:33 +0700

    Regular commit

---
+++ digi/test.py
@@ -0,0 +1,5 @@
+print('Hello world!')
+i = ''
+while i == '':
+    i = input('waiting for your input')
+    
---
+++ digi/testconda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+print('Hello world!')
+i = ''
+while i == '':
+    i = input('waiting for your input')
+    
