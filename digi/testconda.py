82d7b1de9b7024d8f87933f66623c2b1a2a37467
KostyaOs <coolhappygames@gmail.com>
2022-02-07 08:15:57 +0700

    Regular commit

--- digi/testconda.py
+++ digi/testconda.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+print(pd.__version__)
+
+description_path = 'D:/storage/pycharmProjects/project1/digi/description.tsv'
+df = pd.read_csv(
+	description_path, 
+	sep='\t',
+	)
+print(df)
+
+
 print('Hello world!')
 i = ''
 while i == '':
